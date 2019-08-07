6038b4a582d20302ca4e1e032622587d4d9af089
kafun <513112374@qq.com>
2019-08-07 13:43:29 +0800

    压测

---
+++ sell/src/main/java/com/imooc/controller/SeckillController.java
@@ -0,0 +1,43 @@
+package com.imooc.controller;
+
+import com.imooc.service.SeckillService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/skill")
+@Slf4j
+public class SeckillController {
+
+    @Autowired
+    private SeckillService seckillService;
+
+    /**
+     * 查询秒杀活动特价商品的信息
+     *
+     * @param productId
+     * @return
+     */
+    @GetMapping("/query/{productId}")
+    public String query(@PathVariable String productId) throws Exception {
+        return seckillService.querySeckillProductInfo(productId);
+    }
+
+    /**
+     * 秒杀，没有抢到获得“哎哟喂，xxxxx”，抢到了会返回剩余的库存量
+     *
+     * @param productId
+     * @return
+     * @throws Exception
+     */
+    @GetMapping("/order/{productId}")
+    public String skill(@PathVariable String productId) throws Exception {
+        log.info("@skill request,productId:" + productId);
+        seckillService.orderProductMockDiffUser(productId);
+        return seckillService.querySeckillProductInfo(productId);
+    }
+}
---
+++ sell/src/main/java/com/imooc/service/SeckillService.java
@@ -0,0 +1,8 @@
+package com.imooc.service;
+
+public interface SeckillService {
+
+    String querySeckillProductInfo(String productId);
+
+    void orderProductMockDiffUser(String productId);
+}
